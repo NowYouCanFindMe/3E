8a9e8c000079eaa2844e760585da91cdebdb2573
deepak <deepak@sabyatech>
2020-09-18 00:51:16 -0500

    prodData

--- notebooks/loadprod.py
+++ notebooks/loadprod.py
@@ -11,13 +11,41 @@ prod_location = r"Barcode Reflow Oven Scan Times July 2020_1.xlsx"
 dfprod = pd.read_excel(prod_location, index_col=0)
 df1 = pd.read_csv(file_location)
 
-print(df1)
+df = df1
 
-df['Date'] = pandas.to_datetime(df1['Time_Stamp'])
+# display(df1)
 
+df[['Date', 'time']] = df['Time_Stamp'].str.split(" ", expand=True,)
 
-for idx, row in dfprod.iterrows():
-    print(df1[idx, 0])
-    break 
+def my_to_datetime(date_str):
+    #print(date_str)
+    if date_str[8:10] != '24':
+        return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M', errors='ignore')
 
+    date_str = date_str[0:8] + '00' + date_str[10:]
+    return pd.to_datetime(date_str, format='%m/%d/%Y %H:%M', errors='ignore') + \
+           dt.timedelta(days=1)
+df['Time_Stamp'] = df.Time_Stamp.apply(my_to_datetime) 
+
+dateStartMatch = pd.to_datetime('2020-01-01 00:30:00')
+
+for idx, row in df.iterrows():
     
+    print(row['Time_Stamp']) 
+    # np.datetime64('2020-07-01 03:30:00')
+    t2 =  dateStartMatch  + datetime.timedelta(minutes=30)
+    dateEndMatch = t2  #np.datetime64('2020-07-01 04:00:00')
+    print (dateStartMatch, dateEndMatch)
+    
+    dfseg = dfprod[(dfprod['COMPLETION TIME'] > dateStartMatch) 
+               & (dfprod['COMPLETION TIME'] < dateEndMatch)]
+    g = dfseg.groupby(by="LINE")
+    x = g.sum()
+    row['TACTIMESUM'] = x['TAC TIME MIN'].sum()
+    
+    print(row['TACTIMESUM'])
+    
+    dateStartMatch = dateEndMatch
+    
+
+# display(df)
