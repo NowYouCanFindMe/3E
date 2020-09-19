c880278958a4f5e584d3e1f1379e6375f49848cc
NowYouCanFindMe <NowYouCanFindMe@users.noreply.github.com>
2020-09-18 22:30:53 -0400

    Added code changes

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
-// modules
+// components
+import { CompressorComponent } from './compressor/compressor.component';
 import { UsageComponent } from './usage/usage.component';
+import { HomeComponent } from './home/home.component';
 
 // charts
 import { ChartModule } from '@syncfusion/ej2-angular-charts';
-import { CategoryService, LineSeriesService } from '@syncfusion/ej2-angular-charts';
+import { CategoryService, LineSeriesService, BoxAndWhiskerSeriesService, DataLabelService } from '@syncfusion/ej2-angular-charts';
 import { GridModule } from '@syncfusion/ej2-ng-grids';
 import { PageService } from '@syncfusion/ej2-ng-grids';
 
@@ -21,13 +22,19 @@ import { AgGridModule } from 'ag-grid-angular';
 import { HttpClientModule } from '@angular/common/http';
 
 // services
+import { CompressorService } from './shared/services/compressor.service';
 import { MeterService } from './shared/services/meter.service';
+import { OvenService } from './shared/services/oven.service';
+import { OvensComponent } from './ovens/ovens.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    CompressorComponent,
     HomeComponent,
-    UsageComponent
+    UsageComponent,
+    OvensComponent
   ],
   imports: [
     BrowserModule,
@@ -37,7 +44,15 @@ import { MeterService } from './shared/services/meter.service';
     HttpClientModule,
     AgGridModule.withComponents([])
   ],
-  providers: [MeterService, CategoryService, LineSeriesService],
+  providers: [
+    BoxAndWhiskerSeriesService,
+    CompressorService,
+    CategoryService,
+    DataLabelService,
+    LineSeriesService, 
+    MeterService,
+    OvenService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- dashboard/src/app/usage/usage.component.ts
+++ dashboard/src/app/usage/usage.component.ts
@@ -15,14 +15,17 @@ export class UsageComponent implements OnInit {
   // error message
   public errorMsg;
 
-  // chart values
+  // line chart values
   public primaryXAxis: Object;
   public primaryYAxis: Object;
   public chartData: Object[];
+  public chartData2: Object[];
 
-  // line chart
-
-
+  // box chart values
+  public data: Object[];
+  public dataX: Object[];
+  public dataY: Object[];
+  // meter table
     columnDefs = [
     {headerName: 'id', field: 'id', sortable: true, filter: true},
     {headerName: 'Date', field: 'Date', sortable: true, filter: true},
@@ -51,7 +54,8 @@ export class UsageComponent implements OnInit {
   constructor (private meterService: MeterService) { }
   ngOnInit(): void {
       this.loadMeterData();
-
+      this.getTime();
+      
       this.chartData = [
             { month: 'Jan', sales: 35 }, { month: 'Feb', sales: 28 },
             { month: 'Mar', sales: 34 }, { month: 'Apr', sales: 32 },
@@ -61,13 +65,41 @@ export class UsageComponent implements OnInit {
             { month: 'Nov', sales: 25 }, { month: 'Dec', sales: 32 }
       ];
 
+      this.chartData2 = [
+            { month: 'Jan', sales: 45 }, { month: 'Feb', sales: 28 },
+            { month: 'Mar', sales: 54 }, { month: 'Apr', sales: 32 },
+            { month: 'May', sales: 60 }, { month: 'Jun', sales: 33 },
+            { month: 'Jul', sales: 15 }, { month: 'Aug', sales: 51 },
+            { month: 'Sep', sales: 68 }, { month: 'Oct', sales: 34 },
+            { month: 'Nov', sales: 35 }, { month: 'Dec', sales: 32 }
+      ];
+      
+      this.data = [
+                { x: '00:30', y:[200.34, 200.88, 234.90] },
+                { x: '01:00', y:[201.96, 205.20, 231.66] },
+                { x: '01:30', y:[205.74, 200.88, 226.80] },
+                { x: '02:00', y: [204.12, 203.58, 228.42] },
+                { x: '02:30', y: [224.64, 202.5] },
+                { x: '03:00', y: [201.42, 226.26] },
+                { x: '03:30', y: [206.28, 243.00] },
+                { x: '04:00', y: [199.26, 237.60] },
+                { x: '04:30', y: [202.5, 232.20] },
+                { x: '05:00', y: [201.96, 238.14] },
+                { x: '05:30', y: [209.52, 241.38] },
+                { x: '06:00', y: [200.34, 243.00] },
+                { x: '06:30', y: [198.18, 251] },
+                { x: '07:00', y: [262.44, 208.44] },  
+            ];
+
+
       this.primaryXAxis = {
         valueType: 'Category'
       };
 
       this.primaryYAxis = {
-            labelFormat: '${value}K'
+            labelFormat: '{value}'
         };
+      this.initializeCharts();
   }
 
   loadMeterData() {
@@ -75,5 +107,21 @@ export class UsageComponent implements OnInit {
             .subscribe((data: Meter[]) => this.meters = data,
                         error => this.errorMsg = error);
   }
-}
 
+  getTime(){
+    for (let i in this.meters){
+      console.log(i);
+    }
+  }
+
+  initializeCharts(){
+
+    console.log('intialize')
+    console.log(this.meters)
+    for (let value of this.meters){
+      console.log(value)
+    }
+  }
+
+    
+}
